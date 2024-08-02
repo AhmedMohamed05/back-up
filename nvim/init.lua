39297b36ab88947d23c02aa28457e2383a5871db
ahmedmohamed05 <amohamedj29@gmail.com>
2024-08-03 00:42:45 +0300

    nvim config with kikstarter project

---
+++ nvim/init.lua
@@ -0,0 +1,11 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.g.have_nerd_font = true
+
+-- [[ Settings Options ]]
+require("settings.settings")
+
+require("settings.keymaps")
+
+require("plugins.init")
